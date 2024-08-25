d97aa5c4d94dcf1e88831d06d98a4dfca31e6d2a
praskoson <prasinar@gmail.com>
2024-08-26 00:51:47 +0200

    stake authorize tool

---
+++ apps/tools/src/app/spl-token-tools/stake-authorize/page.tsx
@@ -0,0 +1,16 @@
+import { AuthorizeFormsContainer } from "./authorize-forms";
+
+export default function StakeAuthorityManagement() {
+  return (
+    <div className="mx-auto max-w-xl overflow-visible bg-white px-4 pb-5 sm:mb-6 sm:rounded-lg sm:p-6 sm:shadow">
+      <header className="border-b border-gray-200 pb-4">
+        <h1 className="font-display text-3xl font-semibold">
+          Update stake account authority
+        </h1>
+        <p className="mt-4 text-sm text-gray-500">Only for advanced users.</p>
+      </header>
+
+      <AuthorizeFormsContainer />
+    </div>
+  );
+}
