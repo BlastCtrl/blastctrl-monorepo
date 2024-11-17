9f736ac05db3300e776f6c99d1e4db5e84da7596
praskoson <prasinar@gmail.com>
2024-11-17 12:16:29 +0100

    remove extra padding

--- apps/tools/src/app/solana-stake-tools/stake-authorize/page.tsx
+++ apps/tools/src/app/solana-stake-tools/stake-authorize/page.tsx
@@ -2,7 +2,7 @@ import { AuthorizeFormsContainer } from "./authorize-forms";
 
 export default function StakeAuthorityManagement() {
   return (
-    <div className="mx-auto max-w-xl overflow-visible bg-white px-4 pb-5 sm:mb-6 sm:rounded-lg sm:p-6 sm:shadow">
+    <div className="mx-auto max-w-xl overflow-visible bg-white pb-5 sm:mb-6 sm:rounded-lg sm:p-6 sm:shadow">
       <header className="border-b border-gray-200 pb-4">
         <h1 className="font-display text-3xl font-semibold">
           Update stake account authority
