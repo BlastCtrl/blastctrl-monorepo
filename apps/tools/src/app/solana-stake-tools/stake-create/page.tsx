d0b55281085b5afd7c914ff4a2d76f1b2a3a3735
praskoson <prasinar@gmail.com>
2024-11-17 12:16:29 +0100

    remove extra padding

--- apps/tools/src/app/solana-stake-tools/stake-create/page.tsx
+++ apps/tools/src/app/solana-stake-tools/stake-create/page.tsx
@@ -2,7 +2,7 @@ import { StakeAccountForm } from "./stake-acc-form";
 
 export default function CreateStake() {
   return (
-    <div className="mx-auto max-w-2xl overflow-visible bg-white px-4 pb-5 sm:mb-6 sm:rounded-lg sm:p-6 sm:shadow">
+    <div className="mx-auto max-w-2xl overflow-visible bg-white pb-5 sm:mb-6 sm:rounded-lg sm:p-6 sm:shadow">
       <header className="border-b border-gray-200 pb-4">
         <h1 className="font-display text-3xl font-semibold">
           Create stake account
