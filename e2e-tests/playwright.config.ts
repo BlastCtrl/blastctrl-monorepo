84d046490a93fc8a2a0e96b097125c05e1cc772c
praskoson <prasinar@gmail.com>
2025-03-19 18:10:50 +0100

    no retries on CI

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
