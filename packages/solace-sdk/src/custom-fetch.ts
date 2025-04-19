cddce3d62c9fe2fcd166527aae8ce01074073dc4
praskoson <prasinar@gmail.com>
2025-04-19 10:02:28 +0200

    for now, move the solace-sdk into this repo

---
+++ packages/solace-sdk/src/custom-fetch.ts
@@ -0,0 +1,45 @@
+let baseUrl: string
+let defaultHeaders: RequestInit['headers'] = {}
+
+function sanitizeUrl (url: string) {
+  if (url.endsWith('/')) { return url.slice(0, -1) } else { return url }
+}
+
+export const setBaseUrl = (url: string) => {
+  baseUrl = sanitizeUrl(url)
+}
+
+export const setDefaultHeaders = (headers: RequestInit['headers']) => {
+  defaultHeaders = headers
+}
+
+export const customFetch = async <T>(
+  url: string,
+  options: RequestInit
+): Promise<T> => {
+  const requestUrl = `${baseUrl}${url}`
+  const requestHeaders = {
+    ...defaultHeaders,
+    ...options.headers
+  }
+
+  const requestInit: RequestInit = {
+    ...options,
+    headers: requestHeaders,
+  }
+
+  const response = await fetch(requestUrl, requestInit)
+
+  if (!response.ok) {
+    throw Error(await response.text())
+  }
+
+  let data
+  const contentType = response.headers.get('content-type')
+
+  if (contentType?.includes('application/json')) {
+    data = await response.json()
+  }
+
+  return { data, status: response.status, headers: response.headers } as T
+}
